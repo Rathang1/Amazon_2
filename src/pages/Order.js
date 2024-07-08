6d9666c03af8ae07376b8d67610e8161f354cac3
Rathang1 <rathag.rajpal@gmail.com>
2024-07-07 19:35:23 -0500

    build fix

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-function Order({ id, amount, amountShipping, items, timestamp, images }) {
+function Order({ id, amount, amountShipping, items=[], timestamp, images=[] }) {
   return (
     <div className="relative border rounded-md">
         <div className="flex items-center space-x-10 p-5 bg-gray-100 text-sm text-gray-600">
@@ -25,8 +25,8 @@ function Order({ id, amount, amountShipping, items, timestamp, images }) {
 
         <div className="p-5 sm:p-10">
                 <div className="flex space-x-6 overflow-x-auto">
-                    {images.map((image) => (
-                        <img src={image} alt="" className="h-20 object-contain sm:h-32" />
+                    {images.map((image, index) => (
+                        <img key={index} src={image} alt="" className="h-20 object-contain sm:h-32" />
                     ))}
                 </div>
             </div>
